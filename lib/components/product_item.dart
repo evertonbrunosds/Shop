4103c7afa53f0dd3c0672e26afa548c177a7bb0b
evertonbrunosds <evertonbrunosds@gmail.com>
2025-03-13 22:50:17 -0300

    feat: 217

---
+++ lib/components/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          title: Text(product.title, textAlign: TextAlign.center),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+          ),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        child: Image.network(product.imageUrl, fit: BoxFit.cover),
+      ),
+    );
+  }
+}
