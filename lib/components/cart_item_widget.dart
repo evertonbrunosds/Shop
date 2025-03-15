48d5d0d086d41c18a782399a50d24921caed33fb
evertonbrunosds <evertonbrunosds@gmail.com>
2025-03-15 03:57:04 -0300

    feat: 240

--- lib/components/cart_item_widget.dart
+++ lib/components/cart_item_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,18 +10,35 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Padding(
-            padding: const EdgeInsets.all(5),
-            child: FittedBox(child: Text('R\$${cartItem.price}')),
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).colorScheme.error,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: const Icon(Icons.delete, color: Colors.white, size: 40),
+      ),
+      onDismissed: (_) {
+        Provider.of<Cart>(
+          context,
+          listen: false,
+        ).removeItem(cartItem.productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(child: Text('R\$${cartItem.price}')),
+            ),
           ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$${cartItem.price * cartItem.quantity}'),
+          trailing: Text('${cartItem.quantity}x'),
         ),
-        title: Text(cartItem.name),
-        subtitle: Text('Total: R\$${cartItem.price * cartItem.quantity}'),
-        trailing: Text('${cartItem.quantity}x'),
       ),
     );
   }
