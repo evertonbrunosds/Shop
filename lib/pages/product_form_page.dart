b1d5b071e343b73e58b749ac071b4b469f4e9c3e
evertonbrunosds <evertonbrunosds@gmail.com>
2025-03-18 02:11:00 -0300

    feat: adiciona foco e navegação entre campos no formulário de produto

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -8,6 +8,20 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final _nameFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+  final _priceFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+
+  @override
+  void dispose() {
+    _nameFocus.dispose();
+    _descriptionFocus.dispose();
+    _priceFocus.dispose();
+    _imageUrlFocus.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,19 +39,38 @@ class _ProductFormPageState extends State<ProductFormPage> {
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Nome'),
                 textInputAction: TextInputAction.next,
+                focusNode: _nameFocus,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocus);
+                },
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Descrição'),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
                 textInputAction: TextInputAction.next,
+                focusNode: _descriptionFocus,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocus);
+                },
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Preço'),
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
+                focusNode: _priceFocus,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_imageUrlFocus);
+                },
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'URL da Imagem'),
                 textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.url,
+                focusNode: _imageUrlFocus,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).unfocus();
+                },
               ),
               const SizedBox(height: 20),
               ElevatedButton(onPressed: () {}, child: const Text('Salvar')),
