2b6f307b90e67b1fed1c8df03fc23209488837f9
evertonbrunosds <evertonbrunosds@gmail.com>
2025-03-18 04:38:56 -0300

    feat: adiciona método para adicionar produto a partir de dados do formulário

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
@@ -14,6 +16,17 @@ class ProductList with ChangeNotifier {
   void addProduct(final Product product) {
     _products.add(product);
     notifyListeners();
+  }
+
+  void addProductFromData(Map<String, Object> data) {
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'] as String,
+    );
+    addProduct(newProduct);
   }
 
   void removeProduct(final Product product) {
